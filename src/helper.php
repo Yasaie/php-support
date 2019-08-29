b17b209513faac93274fed57c2baa3fb8084ec39
Payam Yasaie <payam.yasaie@outlook.com>
2019-08-29 12:47:58 +0430

    bug fix

--- src/helper.php
+++ src/helper.php
@@ -3,6 +3,6 @@
 if (! function_exists('dot')) {
     function dot($object, $dots, ...$params)
     {
-        \Yasaie\Support\Yalp::dot($object, $dots, ...$params);
+        return \Yasaie\Support\Yalp::dot($object, $dots, ...$params);
     }
 }
